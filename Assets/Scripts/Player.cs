b5758d6ee7745bdfa3a2f2ece8ae17ef1e7eb980
Elian Daiva <elian.alien19@gmail.com>
2015-09-10 16:42:02 +0700

    add random enemy and its physics
    add random enemy and its physics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ public class Player : MonoBehaviour {
 	private Transform myTransform;
 	private Transform movePlayer;
 	public int playerSpeed = 5;
+	private int horizontalBound = 6;
+	private int verticalBoundBottom = -3;
+	private int verticalBoundTop = 10;
 
 	// variable for reusable prefab (gameobject)
 	public GameObject Torpedo; 
@@ -16,22 +19,29 @@ public class Player : MonoBehaviour {
 		myTransform = transform; 
 		// spawn point
 		// position at (-3,-3,-1)
-		myTransform.position = new Vector3 (-3, -3, -1);
+		myTransform.position = new Vector3 (-3, -3, 1);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// move the playe left and right
+		// move the player
 		myTransform.Translate (Vector3.right *playerSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+		myTransform.Translate (Vector3.up * playerSpeed * Input.GetAxis ("Vertical") * Time.deltaTime);
 
 		// make an infinite plane of x
 		// the player goes to more than x = 10 pos, it will endup in x = -10
 		// and vice versa
-		if (myTransform.position.x < -10) {
-			myTransform.position = new Vector3 (10, myTransform.position.y, myTransform.position.z);
-		} else if (myTransform.position.x > 10) {
-			myTransform.position = new Vector3 (-10, myTransform.position.y, myTransform.position.z);
+		if (myTransform.position.x < -horizontalBound) {
+			myTransform.position = new Vector3 (horizontalBound, myTransform.position.y, myTransform.position.z);
+		} else if (myTransform.position.x > horizontalBound) {
+			myTransform.position = new Vector3 (-horizontalBound, myTransform.position.y, myTransform.position.z);
+		}
+
+		if (myTransform.position.y < verticalBoundBottom) {
+			myTransform.position = new Vector3 (myTransform.position.x, verticalBoundBottom, myTransform.position.z);
+		} else if (myTransform.position.y > verticalBoundTop){
+			myTransform.position = new Vector3(myTransform.position.x, verticalBoundTop, myTransform.position.z);
 		}
 
 		// shoot the peluru if space bar pressed
--- Assets/Scripts/musuh.cs
+++ Assets/Scripts/musuh.cs
@@ -3,16 +3,39 @@ using System.Collections;
 
 public class musuh : MonoBehaviour {
 
-	Transform myTransform;
-
-
+	public GameObject enemy;
+	private Transform myTransform;
+	public float minSpeed = 10.0f;
+	public float maxSpeed = 25.0f;
+	int x,y,z;
+	public float currentSpeed;
+	private int horizontalBound = 5;
+	
 	// Use this for initialization
 	void Start () {
-		
+		y = 8;
+		z = 1;
+		x = Random.Range (-horizontalBound, horizontalBound);
+		myTransform = transform;
+		myTransform.position = new Vector3 (x, y, z);
+		currentSpeed = Random.Range (minSpeed,maxSpeed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		x = Random.Range (-horizontalBound, horizontalBound);
+		myTransform.Translate (-Vector3.up * currentSpeed * Time.deltaTime);
+		if (myTransform.position.y < -10) {
+			myTransform.position = new Vector3(x,y,z);
+			currentSpeed = Random.Range(minSpeed,maxSpeed);
+		}
+	}
+
+	void OnTriggerEnter	(Collider collider){
+		// kalo torpedo kena enemy, enemy hancur
+		if (collider.gameObject.){
+			Destroy (this.gameObject);
+		}
+
 	}
 }
